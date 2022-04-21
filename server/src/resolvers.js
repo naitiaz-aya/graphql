a55acb7f2d0bc15bb5de2e8d2f27df6357d27052
naitiaz-aya <bntsaidaya@gmail.com>
2022-04-21 10:52:34 +0000

    graphql

---
+++ server/src/resolvers.js
@@ -0,0 +1,58 @@
+// Resolver est une collection de fonctions qui génèrent une réponse pour une requête GraphQL
+// Il agit comme un gestionnaire de requêtes GraphQL.
+// (root, args, context, info)
+const resolvers = {
+
+  // Query GraphQL est utilisée pour lire ou récupérer des valeurs 
+  Query: {
+    // returns an array of Tracks that will be used to populate the homepage grid of our web client
+    tracksForHome: (_, __, { dataSources }) => {
+      return dataSources.trackAPI.getTracksForHome();
+    },
+
+    // get a single track by ID, for the track page
+    track: (_, { id }, { dataSources }) => {
+      return dataSources.trackAPI.getTrack(id);
+    },
+
+    // get a single module by ID, for the module detail page
+    module: (_, { id }, { dataSources }) => {
+      return dataSources.trackAPI.getModule(id);
+    },
+  },
+
+  // Mutation est utilisée pour écrire ou publier des valeurs.
+  Mutation: {
+    // increments a track's numberOfViews property
+    incrementTrackViews: async (_, { id }, { dataSources }) => {
+      try {
+        const track = await dataSources.trackAPI.incrementTrackViews(id);
+        return {
+          code: 200,
+          success: true,
+          message: `Successfully incremented number of views for track ${id}`,
+          track,
+        };
+      } catch (err) {
+        return {
+          code: err.extensions.response.status,
+          success: false,
+          message: err.extensions.response.body,
+          track: null,
+        };
+      }
+    },
+  },
+
+  Track: {
+    author: ({ authorId }, _, { dataSources }) => {
+      return dataSources.trackAPI.getAuthor(authorId);
+    },
+
+    modules: ({ id }, _, { dataSources }) => {
+      return dataSources.trackAPI.getTrackModules(id);
+    },
+  },
+};
+
+module.exports = resolvers;
